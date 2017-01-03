9821c4adaefb65fa49d44668c99230cc8d84c935
LXL <nearwind_x@163.com>
2017-01-03 13:44:27 +0800

    面向对象高级编程基础

--- OOP/定制类.py
+++ OOP/定制类.py
@@ -61,3 +61,49 @@ class Fib3(object):
 #照这样处理之后就可以对class进行切片操作了
 f3 = Fib3()
 print(f3[0:5])
+
+#正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错，比如定义Studdent类
+class Student2(object):
+    def __init__(self):
+        self.name = 'LXL'
+
+#当类有当前的属性或方法时，没问题，如果没有的话就会报错了
+s2 = Student2()
+print(s2.name)
+#print(s2.score) #因为class没有这个属性或方法  所以程序运行的时候回报错
+#要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态的返回一个属性，修改之后如下
+class Student3(object):
+    def __init__(self):
+        self.name = 'LXL'
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 'you have got a 0'
+
+s3 = Student3()
+print(s3.score)
+
+#当然也可以返回一个函数，只是需要改变一下调用的方式
+class Student4(object):
+    def __init__(self):
+        self.name = 'LXL'
+    def __getattr__(self, attr):
+        if attr == 'age':
+            return lambda: 23
+        raise AttributeError('\'student\' object has no attribute \'%s\'' % attr)
+s4 = Student4()
+print(s4.age())
+
+#一个对象实例可以有自己的属性或者方法，当我们调用方法时，我们用instance.method()来调用，能不能直接在实例本身上直接调用呢？在Python中，答案是肯定的
+#任何类，只需要定义一个__call__方法，就可以直接对实例进行调用，请看实例
+class Student5(object):
+    def __init__(self):
+        self.name = 'LXL'
+    def __call__(self, *args, **kwargs):
+        print('My name is %s' % self.name)
+
+s5 = Student5()
+s5()  #这样定义的话就可以直接调用实例本身
+
+
+#如何去判断一个对象是函数还是对象呢，可以通过callable方法来判定，能被调用就是一个callable对象。通过callable对象我们就能判定对象是不是可调用的对象
+print(callable([1,2,3,4]))
