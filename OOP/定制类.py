c20f0b0507078b6ff5db0bf82525fcf26b30dec2
LXL <nearwind_x@163.com>
2017-01-03 12:28:25 +0800

    面向对象高级编程基础

--- OOP/定制类.py
+++ OOP/定制类.py
@@ -3,4 +3,23 @@ class Student(object):
         self.name = name
     def __str__(self):
         return 'Student Obkect (name: %s)' % self.name
+    __repr__ = __str__
 print(Student('LXL'))
+s = Student('LXL')
+s
+
+#如果想让一个类能够被for...in循环遍历，就必须实现一个__iter__方法,使用该方法会返回一个可迭代的对象，然后Python的for循环就会不断的调用该迭代对象的__next__()方法拿到循环的下一个值,直到遇到了stopIteration后退出
+
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 100000:
+            raise StopIteration()
+        return self.a
+
+for x in Fib():
+    print(x)
