0a4201bf2c80705a8dbd93cf59a62da6e7e4c2ae
xianglinliu <nearwind_x@163.com>
2017-01-05 19:39:26 +0800

    something

--- 错误和调试/错误处理.py
+++ 错误和调试/错误处理.py
@@ -11,3 +11,15 @@ def bar():
         print('Error')
     else:
         pass
+
+#程序运行的过程中如果发生了错误，就可以事先约定返回一个错误代码，这样，就可以知道是否有错，以及出错的原因
+#1、try机制
+try:
+    print('try...')
+    r = 10 / 0
+    print('result:', r)
+except ZeroDivisionError as e:
+    print('except:', e)
+finally:
+    print('finally...')
+print('END')
