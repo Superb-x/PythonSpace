59403dc5d849c3d73addbb3b06fa69c73d1f7f86
LXL <nearwind_x@163.com>
2017-01-06 17:06:56 +0800

    IO编程基础

---
+++ IO编程/StringIOBytesIO.py
@@ -0,0 +1,20 @@
+#很多时候数据不一定是文件，也可以在内存中读写 StringIO
+
+from io import StringIO
+f = StringIO()
+f.write('hello')
+f.write(' ')
+f.write('world!')
+print(f.getvalue())
+#getvalue 方法用来获得写入后的str
+#要读取StringIO，可以用一个str初始化StringIO，然后像文件一样读取
+
+f2 = StringIO('Hello\nHi\nGoodbye!')
+while True:
+    s = f2.readline()
+    if s == '':
+        break
+    print(s.strip())
+
+#StringIO只能操作str，要操作二进制数就只能靠BytesIO了
+
