2a8281d619abec0cfbddf3df5edd0b8f51215565
xianglinliu <nearwind_x@163.com>
2017-01-07 22:25:55 +0800

    进程与线程

--- 进程和线程/pool进程池.py
+++ 进程和线程/pool进程池.py
@@ -17,5 +17,7 @@ if __name__ == '__main__':
         p.apply_async(long_time_task, args=(i,))
     print('Wating for all subprocess done...')
     p.close()
-    p.join()
+    p.join() #join()方法可以等待子进程结束之后再继续往下运行，通常用于进程间的同步
     print('All process has done')
+
+#从结果可以看出   task0,1,2,3是同时执行的，而task 4要等待前面某个task完成之后才能执行，因为我现在的电脑只有四个核心....
