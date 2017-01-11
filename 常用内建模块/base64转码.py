1199ddc3c59aabaed0037848fe3f195440aa2efc
LXL <nearwind_x@163.com>
2017-01-11 10:09:04 +0800

    base64

--- 常用内建模块/base64转码.py
+++ 常用内建模块/base64转码.py
@@ -1,1 +1,8 @@
-str = 'http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431954588961d6b6f51000ca4279a3415ce14ed9d709000'
+import base64, sys
+str = 'http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c'
+
+s = base64.urlsafe_b64decode(str)
+print(s)
+
+f = open('test.bmp', 'rb')
+print(f.read())
