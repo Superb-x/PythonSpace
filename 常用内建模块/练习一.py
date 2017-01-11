5e48967873f548e6bf018b29d680c2a160454bf4
LXL <nearwind_x@163.com>
2017-01-11 10:50:53 +0800

    哈希算法

---
+++ 常用内建模块/练习一.py
@@ -0,0 +1,22 @@
+import hashlib
+
+db = {
+    'michael': 'e10adc3949ba59abbe56e057f20f883e',
+    'bob': '878ef96e86145580c38c87f0410ad153',
+    'alice': '99b1c2188db85afee403b1536010c2c9'
+}
+
+def login(user, password):
+    #先转码
+    md5 = hashlib.md5()
+    md5.update(password.encode('utf-8'))
+    pwd = md5.hexdigest()
+    if db[user] == pwd:
+        print('True')
+        return True
+    else:
+        print('False')
+        return False
+
+if __name__ == '__main__':
+    login('bob', '123456')
