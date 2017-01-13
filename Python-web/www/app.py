f0cd2ba549ec4ed1a775c7e68d5cbf67bec33f1a
LXL <nearwind_x@163.com>
2017-01-13 14:31:53 +0800

    PythonWEB

---
+++ Python-web/www/app.py
@@ -0,0 +1,72 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time, aiomysql
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Index</h1>', content_type='text/html', charset='utf-8')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9001)
+    logging.info('server started at http://127.0.0.1:9001...')
+    return srv
+
+#创建连接池
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf-8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+#SELECT语句
+@asyncio.coroutine
+def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    with (yield from __pool) as conn:
+        cur = yield from conn.cursor(aiomysql.DictCursor)
+        yield from cur.excute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = yield from cur.fetchmany(size)
+        else:
+            rs = yield from cur.fetchall()
+        yield from cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+@asyncio.coroutine
+def execute(sql, args):
+    log(sql)
+    with (yield from __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+#编写简单的ORM
+
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
